58fe31bfc8efe63d42923f934cff760c9b4b53c1
Erdei Tamás <102031387+jeti81@users.noreply.github.com>
2025-01-09 22:08:18 +0100

    Add files via upload

---
+++ Main/mainscript.js
@@ -0,0 +1,21 @@
+/* document.querySelector('.search-icon').addEventListener('click', function() {
+    const searchInput = document.querySelector('.search-input');
+    searchInput.focus();
+}); */
+
+
+document.getElementById('home').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
+
+document.getElementById('explore').addEventListener('click', function() {
+    
+});
+
+document.getElementById('appointment').addEventListener('click', function() {
+    
+});
+
+document.getElementById('login').addEventListener('click', function() {
+
+});
